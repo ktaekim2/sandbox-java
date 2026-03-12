7ab5f81ff43262dd25e5adb52ed4e21490ba4273
kt.kim <kt.kim@seculayer.com>
2026-03-12 13:02:07 +0900

    해시맵 구현 연습

--- src/main/java/io/github/ktaekim2/sandbox/MyHashMap.java
+++ src/main/java/io/github/ktaekim2/sandbox/MyHashMap.java
@@ -40,13 +40,16 @@ public class MyHashMap {
     }
 
     public int get(int key) {
-        Node cur = buckets[index(key)];
+        int idx = index(key);
+        Node cur = buckets[idx];
+
         while (cur != null) {
             if (cur.key == key) {
                 return cur.value;
             }
             cur = cur.next;
         }
+
         return -1;
     }
 
@@ -56,16 +59,21 @@ public class MyHashMap {
         Node prev = null;
 
         while (cur != null) {
+
             if (cur.key == key) {
                 if (prev == null) {
                     buckets[idx] = cur.next;
                 } else {
                     prev.next = cur.next;
                 }
+
                 return;
             }
+
             prev = cur;
             cur = cur.next;
+
         }
+
     }
 }
--- src/test/java/io/github/ktaekim2/sandbox/MyHashMapTest.java
+++ src/test/java/io/github/ktaekim2/sandbox/MyHashMapTest.java
@@ -8,13 +8,34 @@ public class MyHashMapTest {
 
     @Test
     public void put_must_work() {
-        // given
-        MyHashMap map = new MyHashMap();
-
-        // when
-        map.put(1, 1);
+        // given & when
+        MyHashMap map = initMyHashMap();
 
         // then
         assertThat(map.get(1)).isEqualTo(1);
+    }
+
+    private MyHashMap initMyHashMap() {
+        MyHashMap map = new MyHashMap();
+
+        for (int i = 0; i < 5; i++) {
+            map.put(i, i);
+        }
+
+        return map;
+
+    }
+
+    @Test
+    public void remove_must_work() {
+        // given & when
+        MyHashMap map = initMyHashMap();
+
+        // when
+        map.remove(1);
+
+        // then
+        assertThat(map.get(1)).isEqualTo(-1);
+
     }
 }
